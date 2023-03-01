a74cb13e0e2804680547b784e7ea6a025e12c2e2
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-03-01 10:59:12 +0000

    Add components

--- client/src/App.js
+++ client/src/App.js
@@ -1,87 +1,44 @@
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Spinner from "./components/Spinner";
+import Todos from "./components/Todos";
 
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3030/jsonstore/todos')
+      .then(res => res.json())
+      .then(data => 
+        setTodos(Object.values(data))
+        );
+  }, []);
+
   return (
     <div>
-   
-  <header className="navigation-header">
-    <span className="navigation-logo">
-      <img src="/static/images/todo-icon.png" alt="todo-logo" />
-    </span>
-    <span className="spacer"></span>
-    <span className="navigation-description">Todo List</span>
-  </header>
+
+      <Header />
+
+      <main className="main">
 
 
-  <main className="main">
+        <section className="todo-list-container">
+          <h1>Todo List</h1>
 
+          <div className="add-btn-container">
+            <button className="btn">+ Add new Todo</button>
+          </div>
 
-    <section className="todo-list-container">
-      <h1>Todo List</h1>
+          <div className="table-wrapper">
 
-      <div className="add-btn-container">
-        <button className="btn">+ Add new Todo</button>
-      </div>
+            {/*} <Spinner /> */}
 
-      <div className="table-wrapper">
-
-      
-     
-
-      
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="table-header-task">Task</th>
-              <th className="table-header-status">Status</th>
-              <th className="table-header-action">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-
-           
-            <tr className="todo is-completed">
-              <td>Give dog a bath</td>
-              <td>Complete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-          
-            <tr className="todo is-completed">
-              <td>Do laundry</td>
-              <td>Complete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-            <tr className="todo">
-              <td>Vacuum floor</td>
-              <td>Incomplete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-     
-            <tr className="todo">
-              <td>Fill gas tank</td>
-              <td>Incomplete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </section>
-  </main>
-
-
-  <footer className="footer">
-    <p>Copyright © designed by Mihail Valkov</p>
-  </footer>
+            <Todos todos={todos} />
+          </div>
+        </section>
+      </main>
+      <Footer />
     </div>
   );
 }
