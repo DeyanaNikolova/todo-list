ff7642302c675788ed62c911164f396bb370a1c5
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-03-01 09:44:21 +0000

    Add resources

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div>
+   
+  <header className="navigation-header">
+    <span className="navigation-logo">
+      <img src="/static/images/todo-icon.png" alt="todo-logo" />
+    </span>
+    <span className="spacer"></span>
+    <span className="navigation-description">Todo List</span>
+  </header>
 
+
+  <main className="main">
+
+
+    <section className="todo-list-container">
+      <h1>Todo List</h1>
+
+      <div className="add-btn-container">
+        <button className="btn">+ Add new Todo</button>
+      </div>
+
+      <div className="table-wrapper">
+
+      
+     
+
+      
+        <table className="table">
+          <thead>
+            <tr>
+              <th className="table-header-task">Task</th>
+              <th className="table-header-status">Status</th>
+              <th className="table-header-action">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+
+           
+            <tr className="todo is-completed">
+              <td>Give dog a bath</td>
+              <td>Complete</td>
+              <td className="todo-action">
+                <button className="btn todo-btn">Change status</button>
+              </td>
+            </tr>
+
+          
+            <tr className="todo is-completed">
+              <td>Do laundry</td>
+              <td>Complete</td>
+              <td className="todo-action">
+                <button className="btn todo-btn">Change status</button>
+              </td>
+            </tr>
+
+            <tr className="todo">
+              <td>Vacuum floor</td>
+              <td>Incomplete</td>
+              <td className="todo-action">
+                <button className="btn todo-btn">Change status</button>
+              </td>
+            </tr>
+     
+            <tr className="todo">
+              <td>Fill gas tank</td>
+              <td>Incomplete</td>
+              <td className="todo-action">
+                <button className="btn todo-btn">Change status</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </main>
+
+
+  <footer className="footer">
+    <p>Copyright © designed by Mihail Valkov</p>
+  </footer>
     </div>
   );
 }
